eeea1b08452a21d29bee5127787d77c31c1378af
somtola <mac@somtola.local>
2024-08-27 16:57:48 +0700

    new update

--- src/db/BlogServices.js
+++ src/db/BlogServices.js
@@ -6,20 +6,14 @@ export function BlogService(){
    return  [
         {
             id: 1,
-            title: 'Does my company call for an audit after three years of business operation ?',
-            content: 'What kind of questions will I be asked during external auditing?',
             image: imgCard_1,
         },
         {
             id: 2,
-            title: 'Lorem Ipsum',
-            content: 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
             image: imgCard_2,
         },
         {
             id: 3,
-            title: 'Lorem Ipsum',
-            content: 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
             image: imgCard_3,
         },
     ]
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -16,15 +16,15 @@ const Blog = () => {
       <div className='bg-[#ffffff] w-full max-w-screen-lg xl:max-w-screen-xl mx-auto my-10'>
         <div className='grid grid-cols-12 justify-center items-center gap-[2vw] lg:gap-[1vw] px-5'>
           {items.map((blogs) => (
-            <Link to={"/blog/blog-details"} data-aos="fade-up" key={blogs.id}
+            <div data-aos="fade-up" key={blogs.id}
               className='relative col-span-12 md:col-span-6 lg:col-span-4 grid w-full mx-auto min-h-[30rem] items-end overflow-hidden rounded-md group'>
               <img src={blogs.image} className='absolute w-full h-full object-cover object-center inset-0 group-hover:scale-[1.2] duration-500' />
-              <div className='absolute bg-black/50 inset-0'></div>
+              {/* <div className='absolute bg-black/50 inset-0'></div>
               <div className='relative flex flex-col justify-end p-5'>
                 <h1 className='text-[#ffffff] font-bold text-2xl mb-2'>{blogs.title}</h1>
                 <p className='text-[#ffffff] font-semibold text-md'>{blogs.content}</p>
-              </div>
-            </Link>
+              </div> */}
+            </div>
           ))}
         </div>
       </div>
